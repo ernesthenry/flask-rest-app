b695bbc01432826dddf1667cad02b343d6b281a2
ernesthenry <henry38ernest@gmail.com>
2020-04-21 15:27:34 +0300

    set up applivation database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from .models import setup_db, Plant
 from flask_cors import CORS
 
 
@@ -6,6 +7,7 @@ from flask_cors import CORS
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    setup_db(app)
     CORS(app)
     # cors = CORS(app, resources={r"/api/*": {"origins": "*"}}) # resource  specific
 
